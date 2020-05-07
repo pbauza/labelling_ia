b2f5cdd9c9b23cd569636cfa478352c15b9a451d
pbauza <pjbauza@gmail.com>
2020-05-07 02:27:27 +0200

    retrieval by color and shape

--- my_labeling.py
+++ my_labeling.py
@@ -2,11 +2,12 @@ __authors__ = 'TO_BE_FILLED'
 __group__ = 'TO_BE_FILLED'
 
 import numpy as np
-import Kmeans
+import Kmeans_Students
 import KNN
 from utils_data import read_dataset, visualize_k_means, visualize_retrieval
 import matplotlib.pyplot as plt
-import cv2
+#import cv2
+from operator import  itemgetter
 
 if __name__ == '__main__':
 
@@ -17,13 +18,75 @@ if __name__ == '__main__':
     #List with all the existant classes
     classes = list(set(list(train_class_labels) + list(test_class_labels)))
 
-
-
 ## You can start coding your functions here
 
+def Retrieval_by_color(images, labels, colors):
+    c = np.empty([len(labels)], np.object)
+    c[:] = np.nan
+    p = np.empty([len(labels)], np.object)
+    p[:] = np.nan
 
+    for i in range(len(labels)):
+        c[i], p[i] = np.unique(labels[i], return_counts=True)
+        p[i] = p[i] / len(labels[i])
 
+    image_list = dict()
+    for index, i in enumerate(images):
+        m = 0
+        found = False
+        while m < len(colors) and found is False:
+            if colors[m] not in labels[index]:
+                found = True
+                if index in image_list.keys():
+                    image_list.pop(index, None)
+            else:
+                if index not in image_list.keys():
+                    image_list[index] = 0
+                t = np.argwhere(c[index] == colors[m])
+                image_list[index] += p[index][np.argwhere(c[index] == colors[m])]
+                m += 1
 
+    image_list = sorted(image_list.items(), key=itemgetter(1), reverse=True)
+    return_list = list()
 
+    for i in range(len(image_list)):
+        return_list.append(images[image_list[i][0]])
 
+    visualize_retrieval(return_list, 2)
+    return return_list
+
+def Retrieval_by_shape(images, labels, shapes):
+    c = np.empty([len(labels)], np.object)
+    c[:] = np.nan
+    p = np.empty([len(labels)], np.object)
+    p[:] = np.nan
+
+    for i in range(len(labels)):
+        c[i], p[i] = np.unique(labels[i], return_counts=True)
+        p[i] = p[i] / len(labels[i])
+
+    image_list = dict()
+    for index, i in enumerate(images):
+        m = 0
+        found = False
+        while m < len(shapes) and found is False:
+            if shapes[m] not in labels[index]:
+                found = True
+                if index in image_list.keys():
+                    image_list.pop(index, None)
+            else:
+                if index not in image_list.keys():
+                    image_list[index] = 0
+                t = np.argwhere(c[index] == shapes[m])
+                image_list[index] += p[index][np.argwhere(c[index] == shapes[m])]
+                m += 1
+
+    image_list = sorted(image_list.items(), key=itemgetter(1), reverse=True)
+    return_list = list()
+
+    for i in range(len(image_list)):
+        return_list.append(images[image_list[i][0]])
+
+    visualize_retrieval(return_list, 2) #no sabem per que no funciona si esta igual que laltre
+    return return_list
 
